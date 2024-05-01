733018adc86f727d68c4fe83d5fec30a32b69b9c
root <root@ip-172-31-72-115.ec2.internal>
2024-05-01 14:17:42 +0000

    test code edit-1

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var chai = require('chai');
 var io = require('socket.io-client')
 var app = require('../index');
-var client = io('http://3.238.254.226:4141');
+var client = io('http://localhost:4141');
  
 console.log('Server started...Waiting for client connection...');
 it('Client connected...', function(done) {
