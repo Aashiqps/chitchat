bc87f7a9e94da97b9366c15e2be53fed1e8be382
root <root@ip-172-31-72-115.ec2.internal>
2024-05-01 12:49:19 +0000

    test code modified

--- test/test.js
+++ test/test.js
@@ -1,11 +1,34 @@
 var chai = require('chai');
-var io = require('socket.io-client')
-var app = require('../index');
-var client = io('http://3.238.254.226:4141');
- 
-console.log('Server started...Waiting for client connection...');
-it('Client connected...', function(done) {
-  client.on('connect', function (data) {
-    done();
-  });
+var expect = chai.expect;
+var io = require('socket.io-client');
+
+// Replace 'http://3.238.254.226:4141' with your server address
+var serverAddress = 'http://3.238.254.226:4141';
+
+describe('Socket Connection', function() {
+    var client;
+
+    beforeEach(function(done) {
+        // Connect to the server before each test
+        client = io.connect(serverAddress);
+        client.on('connect', function() {
+            console.log('Connected to server');
+            done();
+        });
+    });
+
+    afterEach(function(done) {
+        // Disconnect from the server after each test
+        if (client.connected) {
+            console.log('Disconnecting from server');
+            client.disconnect();
+        }
+        done();
+    });
+
+    it('should connect to the server', function(done) {
+        // Check if the client is connected
+        expect(client.connected).to.be.true;
+        done();
+    });
 });
